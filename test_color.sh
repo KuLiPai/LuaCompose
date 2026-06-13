ba463a1d8ad592ca2f8c60aca809b51b5c360bdf
KuLiPai <yun.svip@gmail.com>
2026-06-13 11:34:36 +0800

    Clean up test files

--- test_color.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp app/src/main/assets/main.lua /sdcard/Android/data/com.kulipai.luacompose/files/main.lua
-adb logcat -c
-am start -n com.kulipai.luacompose/.MainActivity
-sleep 2
-adb logcat -d | grep -E "AndroidRuntime|LUA_ANIM|Exception|Error"
--- test_double.java
+++
@@ -1,5 +0,0 @@
-public class test_double {
-    public static void main(String[] args) {
-        System.out.println((int)4284722393.0);
-    }
-}
